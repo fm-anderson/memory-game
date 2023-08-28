5ea3cfb06b5f30ac99f768ed05346b91a661301e
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2023-08-28 12:29:14 -0400

    fix: knuth shuffle replacing Math.random

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10,3 +10,17 @@ export const randomKey = (length) => {
   }
   return result;
 };
+
+export const shuffleArray = (array) => {
+  let currentIndex = array.length,
+    randomIndex;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+};
